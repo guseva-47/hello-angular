ddc370befcfa19febc4d7a7602b59726a005bc09
Aleksandra <guseva-47@yandex.ru>
2022-01-09 01:57:34 +0700

    removal of components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ItemComponent } from './item/item.component';
-import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ItemComponent,
-    HeaderComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
